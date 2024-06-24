ee6a1d05a8efa9eb2efecbcba64cfc33f4a636d2
Prisha Gupta <prishapg101@gmail.com>
2024-06-24 18:40:35 +0530

    Added parsing of type aliases

---
+++ rocket-chatter-ingestion-server/src/objects/typeAlias.ts
@@ -0,0 +1,25 @@
+import { namedTypes } from "ast-types"
+import { DBNode } from "./dbNode"
+import { TypeAnnotation } from "./typeAnnotation"
+
+export namespace TypeAlias {
+	export function Handle(n: namedTypes.TSTypeAliasDeclaration) {
+		const node = new DBNode(n.id?.name.toString() ?? "", "TypeAlias", "")
+
+		// Check for type parameters
+		const typeParameters: string[] = []
+		for (const p of n.typeParameters?.params ?? []) {
+			typeParameters.push((p.name as any).name)
+		}
+
+		// Check for external references
+		node.pushUse(
+			...TypeAnnotation.flatten(n.typeAnnotation).map((name) => ({
+				name,
+				type: "type",
+			}))
+		)
+
+		return node
+	}
+}
