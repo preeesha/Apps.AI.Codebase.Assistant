f6e40a5e8b0190f29793b978a763cf4206c58428
Prisha Gupta <prishapg101@gmail.com>
2024-06-25 18:22:02 +0530

    Added support for line & column numbers in `TreeNode`

--- rocket-chatter-ingestion-server/src/process/prepare/processor/core/treeNode.ts
+++ rocket-chatter-ingestion-server/src/process/prepare/processor/core/treeNode.ts
@@ -2,19 +2,35 @@ export class TreeNode {
 	name: string = ""
 	type: string = ""
 	body: string = ""
+
+	lineNumber: number
+	columnNumber: number
+
 	uses: {
 		name: string
 		type: string
 	}[] = []
 
-	constructor(name: string, type: string, body: string) {
+	constructor(
+		name: string,
+		type: string,
+		body: string,
+		lineNumber: number,
+		columnNumber: number
+	) {
 		this.name = name
 		this.type = type
 		this.body = body
+		this.lineNumber = lineNumber
+		this.columnNumber = columnNumber
 	}
 
 	toString() {
 		return `${this.name} (${this.type})`
+	}
+
+	getID(): string {
+		return `${this.name}:${this.type}:${this.lineNumber}:${this.columnNumber}`
 	}
 
 	pushUse(...uses: { name: string; type: string }[]) {
