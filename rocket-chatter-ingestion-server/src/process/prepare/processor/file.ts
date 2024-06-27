91f912b51b96defdf0ebd5c65fb710834ba2059f
Prisha Gupta <prishapg101@gmail.com>
2024-06-27 14:21:17 +0530

    Added parsing of imports & replacement of the names nodes used from other file with their system generated ID

--- rocket-chatter-ingestion-server/src/process/prepare/processor/file.ts
+++ rocket-chatter-ingestion-server/src/process/prepare/processor/file.ts
@@ -1,5 +1,6 @@
 import { parse } from "@typescript-eslint/typescript-estree"
 import { namedTypes } from "ast-types"
+import path from "path"
 
 import { DBNode } from "../../../core/dbNode"
 import { ISourceFile } from "../sourceFile.types"
@@ -19,34 +20,68 @@ export class FileProcessor implements IFileProcessor {
 		const fileContent = sourceFile.read()
 		const ast = parse(fileContent)
 
-		const imports = ast.body.filter((node) =>
-			namedTypes.ImportDeclaration.check(node)
-		)
-
 		let treeNodes: TreeNode[] = []
 		for (let node of ast.body) {
-			if (namedTypes.ExportNamedDeclaration.check(node)) {
+			if (namedTypes.ExportNamedDeclaration.check(node))
 				node = (node as any).declaration
-			}
 
-			if (namedTypes.FunctionDeclaration.check(node)) {
+			if (namedTypes.FunctionDeclaration.check(node))
+				/* Functions */
 				treeNodes.push(Functions.Handle(node))
-			} else if (namedTypes.TSInterfaceDeclaration.check(node)) {
+			else if (namedTypes.TSInterfaceDeclaration.check(node))
+				/* Interfaces */
 				treeNodes.push(Interface.Handle(node))
-			} else if (namedTypes.TSTypeAliasDeclaration.check(node)) {
+			else if (namedTypes.TSTypeAliasDeclaration.check(node))
+				/* Type Aliases */
 				treeNodes.push(TypeAlias.Handle(node))
-			} else if (namedTypes.TSEnumDeclaration.check(node)) {
+			else if (namedTypes.TSEnumDeclaration.check(node))
+				/* Enums */
 				treeNodes.push(Enums.Handle(node))
-			} else if (namedTypes.ClassDeclaration.check(node)) {
+			else if (namedTypes.ClassDeclaration.check(node))
+				/* Classes */
 				treeNodes.push(Classes.Handle(node))
-			} else if (namedTypes.TSModuleDeclaration.check(node)) {
+			else if (namedTypes.TSModuleDeclaration.check(node))
+				/* Namespaces */
 				treeNodes.push(Namespaces.Handle(node))
-			} else if (namedTypes.VariableDeclaration.check(node)) {
+			else if (namedTypes.VariableDeclaration.check(node))
+				/* Variables */
 				treeNodes.push(...VariableDeclarations.Handle(node))
+		}
+
+		// Resolve imports
+		const parsedImports = new Map<string, string>() // { importName: absolutePath }
+		{
+			const imports = ast.body
+				.filter((node) => namedTypes.ImportDeclaration.check(node)) // Filter out all non-import nodes
+				.filter((node) => (node as any).source.value.startsWith(".")) // Filter out all library/non-relative imports
+			for (const i of imports) {
+				const absolutePath = path.resolve(
+					path.dirname(sourceFile.getFullPath()),
+					(i as any).source.value + ".ts"
+				)
+				const importName = (i as any).specifiers[0].local.name
+
+				parsedImports.set(importName, absolutePath)
 			}
 		}
+
+		// Replace import names with absolute paths
 		for (const treeNode of treeNodes) {
-			sourceFile.registerSyntax(treeNode)
+			treeNode.sourceFilePath = sourceFile.getFullPath()
+			treeNode.uses = treeNode.uses
+				.filter((x) => x.name)
+				.map((x) => {
+					if (parsedImports.has(x.name)) {
+						x.name = parsedImports.get(x.name)!
+					}
+					return x
+				})
+		}
+
+		// Add the nodes to the global reference
+		for (const treeNode of treeNodes) {
+			const dbNode = DBNode.fromTreeNode(treeNode)
+			nodesRef[dbNode.id] = dbNode
 		}
 	}
 }
