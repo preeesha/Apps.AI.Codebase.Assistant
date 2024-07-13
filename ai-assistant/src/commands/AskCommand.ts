0d68a4f3d2db7f5b5f220d5bdb6312de4736647d
Prisha Gupta <prishapg101@gmail.com>
2024-07-13 15:17:07 +0530

    Moulded the `/rcc-ask` command to the new local entities

--- ai-assistant/src/commands/AskCommand.ts
+++ ai-assistant/src/commands/AskCommand.ts
@@ -7,14 +7,63 @@ import {
     ISlashCommand,
     SlashCommandContext,
 } from "@rocket.chat/apps-engine/definition/slashcommands";
+
+import { Neo4j } from "../core/db/neo4j";
+import { MiniLML6 } from "../core/embeddings/minilml6";
+import { Llama3_70B } from "../core/llm/llama3_70B";
+import { Prompts } from "../core/prompt/prompts";
+import { Query } from "../core/query";
 import { handleCommandResponse } from "../utils/handleCommandResponse";
-import { requestServer } from "../utils/requestServer";
 
 export class AskCommand implements ISlashCommand {
     public command = "rcc-ask";
     public i18nParamsExample = "";
     public i18nDescription = "";
     public providesPreview = false;
+
+    private async process(http: IHttp, query: string): Promise<string | null> {
+        const db = new Neo4j(http);
+        const llm = new Llama3_70B(http);
+        const embeddingModel = new MiniLML6(http);
+
+        /**
+         * ---------------------------------------------------------------------------------------------
+         * STEP 1:
+         * Extract the possible keywords from the user's query
+         * ---------------------------------------------------------------------------------------------
+         */
+        const keywords = await Query.getDBKeywordsFromQuery(llm, query);
+        console.log("KEYWORDS", keywords);
+        if (!keywords.length) return null;
+
+        /**
+         * ---------------------------------------------------------------------------------------------
+         * STEP 2:
+         * Query the database to find the nodes names of which are similar to what user has requested
+         * ---------------------------------------------------------------------------------------------
+         */
+        const results = await Query.getCodeNodesFromKeywords(
+            db,
+            embeddingModel,
+            keywords
+        );
+        console.log("RESULTS", results);
+        if (!results.length) return null;
+
+        /**
+         * ---------------------------------------------------------------------------------------------
+         * STEP 3:
+         * Generate the answer and diagram for the user's query given the nodes data
+         * ---------------------------------------------------------------------------------------------
+         */
+        const answer = await llm.ask(
+            Prompts.makeAskPrompt(JSON.stringify(results), query)
+        );
+        console.log("ANSWER", answer);
+        if (!answer) return null;
+
+        return answer;
+    }
 
     public async executor(
         context: SlashCommandContext,
@@ -35,13 +84,8 @@ export class AskCommand implements ISlashCommand {
             this.command
         );
 
-        const res = await requestServer(http, "/ask", { query });
-        if (!res) {
-            await sendEditedMessage("❌ No references found!");
-            return;
-        }
-
-        const data = res as string;
-        await sendEditedMessage(data);
+        let res = await this.process(http, query);
+        if (!res) res = "❌ Unable to process your query";
+        await sendEditedMessage(res);
     }
 }
