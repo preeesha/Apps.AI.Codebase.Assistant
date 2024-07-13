3bd69f3a890962ea11bf327724ac84881fa85fdc
Prisha Gupta <prishapg101@gmail.com>
2024-07-13 10:43:04 +0530

    Added missing functions from the `IDB` interface

--- ai-assistant/src/core/db/db.types.ts
+++ ai-assistant/src/core/db/db.types.ts
@@ -2,4 +2,12 @@ export interface IDB {
     verifyConnectivity(): Promise<void>;
 
     closeDBConnection(): Promise<void>;
+
+    beginTransaction(): Promise<void>;
+
+    commitTransaction(): Promise<void>;
+
+    rollbackTransaction(): Promise<void>;
+
+    run(query: string, params: any): Promise<any>;
 }
