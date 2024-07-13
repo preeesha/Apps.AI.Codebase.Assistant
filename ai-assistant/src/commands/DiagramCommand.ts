9294999bb73f1fff26e61b9c84cde01c360daa2f
Prisha Gupta <prishapg101@gmail.com>
2024-07-13 15:29:55 +0530

    Moulded the `/rcc-diagram` command to the new local entities

--- ai-assistant/src/commands/DiagramCommand.ts
+++ ai-assistant/src/commands/DiagramCommand.ts
@@ -7,14 +7,73 @@ import {
     ISlashCommand,
     SlashCommandContext,
 } from "@rocket.chat/apps-engine/definition/slashcommands";
+
+import { Neo4j } from "../core/db/neo4j";
+import { renderDiagramToBase64URI } from "../core/diagram";
+import { MiniLML6 } from "../core/embeddings/minilml6";
+import { Llama3_70B } from "../core/llm/llama3_70B";
+import { Prompts } from "../core/prompt/prompts";
+import { Query } from "../core/query";
 import { handleCommandResponse } from "../utils/handleCommandResponse";
-import { requestServer } from "../utils/requestServer";
 
 export class DiagramCommand implements ISlashCommand {
     public command = "rcc-diagram";
     public i18nParamsExample = "";
     public i18nDescription = "";
     public providesPreview = false;
+
+    private async process(http: IHttp, query: string): Promise<string | null> {
+        const db = new Neo4j(http);
+        const llm = new Llama3_70B(http);
+        const embeddingModel = new MiniLML6(http);
+
+        /**
+         * ---------------------------------------------------------------------------------------------
+         * STEP 1:
+         * Extract the possible keywords from the user's query
+         * ---------------------------------------------------------------------------------------------
+         */
+        const keywords = await Query.getDBKeywordsFromQuery(llm, query);
+        console.log("KEYWORDS", keywords);
+        if (!keywords.length) return null;
+
+        /**
+         * ---------------------------------------------------------------------------------------------
+         * STEP 2:
+         * Query the database to find the nodes names of which are similar to what user has requested
+         * ---------------------------------------------------------------------------------------------
+         */
+        const results = await Query.getCodeNodesFromKeywords(
+            db,
+            embeddingModel,
+            keywords
+        );
+        console.log("RESULTS", results);
+        if (!results.length) return null;
+
+        /**
+         * ---------------------------------------------------------------------------------------------
+         * STEP 3:
+         * Generate the diagram code and diagram for the user's query given the nodes data
+         * ---------------------------------------------------------------------------------------------
+         */
+        const diagram = await llm.ask(
+            Prompts.makeDiagramPrompt(JSON.stringify(results), query)
+        );
+        console.log(diagram);
+        if (!diagram) return null;
+
+        const diagramContent = diagram
+            .replace("```mermaid", "")
+            .replace("```", "")
+            .split("<DIAGRAM_START>")[1]
+            .split("<DIAGRAM_END>")[0]
+            .trim();
+        console.log("DIAGRAM:\n", diagramContent);
+
+        const base64Diagram = await renderDiagramToBase64URI(diagramContent);
+        return base64Diagram;
+    }
 
     public async executor(
         context: SlashCommandContext,
@@ -35,13 +94,11 @@ export class DiagramCommand implements ISlashCommand {
             this.command
         );
 
-        const res = await requestServer(http, "/diagram", { query });
-        if (!res) {
+        let res = await this.process(http, query);
+        if (res) {
+            await sendEditedMessage("", [res]);
+        } else {
             await sendEditedMessage("❌ Diagram cannot be generated.");
-            return;
         }
-
-        const data = res as string;
-        await sendEditedMessage("", [data]);
     }
 }
