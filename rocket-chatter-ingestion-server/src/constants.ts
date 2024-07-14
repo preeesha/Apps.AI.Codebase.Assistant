4d11074cd702e4d593f9acfb87a2cbef14a6a26e
Prisha Gupta <prishapg101@gmail.com>
2024-07-14 13:33:51 +0530

    Updated interface for environment variables in `constants.ts`

--- rocket-chatter-ingestion-server/src/constants.ts
+++ rocket-chatter-ingestion-server/src/constants.ts
@@ -1,12 +1,4 @@
 import { configDotenv } from "dotenv"
 configDotenv()
 
-export const PORT = +(process.env["PORT"] ?? 3000)
-
-export const HF_KEY = process.env["HF_KEY"]
-export const OPENAI_KEY = process.env["OPENAI_KEY"]
-
 export const NEO4J_URI = process.env["NEO4J_URI"] ?? ""
-export const NEO4J_USERNAME = process.env["NEO4J_USERNAME"] ?? ""
-export const NEO4J_PASSWORD = process.env["NEO4J_PASSWORD"] ?? ""
-export const NEO4J_DATABASE = process.env["NEO4J_DATABASE"] ?? ""
